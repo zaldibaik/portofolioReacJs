982991e3be0c1d3d4c6d8624daba4923ca9ef0cd
zaldi <zaldi104@smk.belajar.id>
2024-08-20 16:00:48 +0700

    update

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
         <div className="container mx-auto">
           <h2 className="text-4xl font-bold text-gray-800 mb-4">About Me</h2>
           <p className="text-gray-700 leading-relaxed">
-            Halo Aku saya Zaldi, Aku adalah anak pertama yang harus menjadi anak soleh, yang akan menjadi panutan adik adik ku, aku akun membawa kedua orang tuaku selamat dunia dan akhirat, Aminn
+            Halo Aku Zaldi, Aku adalah anak pertama yang harus menjadi anak soleh, yang akan menjadi panutan adik adik ku, aku akun membawa kedua orang tuaku, atau keluargaku selamat dunia dan akhirat, Aminn
           </p>
         </div>
       </section>
