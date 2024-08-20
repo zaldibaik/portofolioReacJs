3e7fcc46fe4c74b6cc3d4a572437e1fc29c9c9da
zaldibaik <zaldi104@smk.belajar.id>
2024-08-20 15:58:05 +0700

    upload

--- src/components/About.js
+++ src/components/About.js
@@ -4,7 +4,7 @@ const About = () => {
         <div className="container mx-auto">
           <h2 className="text-4xl font-bold text-gray-800 mb-4">About Me</h2>
           <p className="text-gray-700 leading-relaxed">
-            I am a passionate web developer with a focus on creating beautiful and functional user experiences...
+            Halo Aku saya Zaldi, Aku adalah anak pertama yang harus menjadi anak soleh, yang akan menjadi panutan adik adik ku, aku akun membawa kedua orang tuaku selamat dunia dan akhirat, Aminn
           </p>
         </div>
       </section>
