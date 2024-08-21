b760c22a39f17cd544def7dfe3f2a0702353b79b
zaldi <zaldi104@smk.belajar.id>
2024-08-21 15:27:25 +0700

    Update

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -6,12 +6,12 @@ const Heading = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div data-aos="fade-up">
           <img
-            className="w-30 h-30 rounded-full object-cover md:p-10 p-20"
+            className="w-30 h-30 rounded-full object-cover md:p-10 p-10"
             src={gambar}
             alt="Descriptive alt text"
           />
         </div>
-        <div className="flex flex-col justify-center items-center md:items-start md:pt-60 pt-10"> {/* Adjusted padding for mobile */}
+        <div className="flex flex-col justify-center items-center md:items-start md:pt-60 pt-10">
           <div className="md:text-1xl">
             <div className="md:text-6xl text-center mb-4 md:mb-8">Halo nama saya Zaldi</div>
             <a
