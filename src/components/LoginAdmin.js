6eef3f0159b846558f242da7742d2f0177f3745d
zaldi <zaldi104@smk.belajar.id>
2024-08-22 22:21:52 +0700

    Update

--- src/components/LoginAdmin.js
+++ src/components/LoginAdmin.js
@@ -77,15 +77,6 @@ const LoginAdmin = () => {
                   >
                     Sign in
                   </button>
-                  <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                    Don’t have an account yet?{" "}
-                    <a
-                      href="#"
-                      class="font-medium text-primary-600 hover:underline dark:text-primary-500"
-                    >
-                      Sign up
-                    </a>
-                  </p>
                 </form>
               </div>
             </div>
